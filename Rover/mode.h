68b36d0c5e6cbfa17c32d83fbf599d84a7ec1610
Matthew McGill <matthew_mcgill@hotmail.com>
2020-08-05 17:32:47 +1000

    IVP mode publishes intermediate WP 1km in desired direction

--- Rover/mode.h
+++ Rover/mode.h
@@ -758,9 +758,19 @@ private:
 
 class ModeIVP : public Mode
 {
+private:
+//    float _desired_heading_cd;  // latest desired heading (in centi-degrees) from pilot
+    float _desired_speed;       // latest desired speed in m/s
+    bool _target_is_throttle;
+    bool have_attitude_target;  // true if we have a valid attitude target
+    uint32_t _des_att_time_ms;  // system time last call to set_desired_attitude was made (used for timeout)
+
 public:
     uint32_t mode_number() const override { return IVP; }
     const char *name4() const override { return "IVP"; }
+
+    float getDesiredHeading() { return _desired_yaw_cd / 100; }
+    float getDesiredSpeed() { return _desired_speed; }
 
     // set desired heading and speed
     void set_desired_heading_and_speed(float yaw_angle_cd, float target_speed) override;
@@ -778,12 +788,5 @@ public:
 
 protected:
     bool _enter(mode_reason_t reason) override;
-
-private:
-    float _desired_heading_cd;  // latest desired heading (in centi-degrees) from pilot
-    float _desired_speed;       // latest desired speed in m/s
-    bool _target_is_throttle;
-    bool have_attitude_target;  // true if we have a valid attitude target
-    uint32_t _des_att_time_ms;  // system time last call to set_desired_attitude was made (used for timeout)
 };
 
