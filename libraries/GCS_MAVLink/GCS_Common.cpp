67129c2cbc6b2891c9e77a203aa9ed53f08a4c07
Matthew McGill <matthew_mcgill@hotmail.com>
2021-09-27 03:06:52 +1000

    Comment our the PID feedback send to stop flood of messages

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -5416,5 +5416,5 @@ GCS &gcs()
 
 void GCS::send_oc_pid_feedback(const char* locn, float desired, float achieved, float output, float ff, float p, float i, float d, float param1, float param2, float param3, float param4)
 {
-    mavlink_msg_oc_pid_feedback_send(MAVLINK_COMM_0, locn, desired, achieved, output, ff, p, i, d, param1, param2, param3, param4);
+//    mavlink_msg_oc_pid_feedback_send(MAVLINK_COMM_0, locn, desired, achieved, output, ff, p, i, d, param1, param2, param3, param4);
 }
